9c8163c56a4d2dd6e77d6018bf1ff99d0389f4eb
shahriarsohan <sohanmock@gmail.com>
2023-03-19 15:44:51 +0600

    management command added to create new superuser on the go

---
+++ accounts/management/command/initadmin.py
@@ -0,0 +1,21 @@
+import environ
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from accounts.models import Accounts
+
+env = environ.Env()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if Accounts.objects.count() == 0:
+            for user in settings.ADMINS:
+                admin = Accounts.objects.create_superuser(
+                    email=env("ADMIN_EMAIL"), password=env("ADMIN_PASSWORD"))
+                admin.is_active = True
+                admin.is_admin = True
+                admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
